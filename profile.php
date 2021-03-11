76d745a7d6cf1de1fd29e7f2a3fdbc01c3a77683
Ercice <scrahmawati99@gmail.com>
2021-03-11 16:01:48 +0800

    commit pertama: project praktikum Suci Rahmawati - 17630316

---
+++ profile.php
@@ -0,0 +1,50 @@
+<?php include "header.php" ?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/17630316.jpg" alt="Suci Rahmawati" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NIK / NIDN :</strong></td>
+                        <td align="left" width="50%">17630316</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Suci Rahmawati</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 30 Desember 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0887 636 9998</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">sucirahmawati1230@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://instagram.com/er.ciii" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/rceee5" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/008-twitter.svg" width="30" alt="twitter">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
